8d6c48967f2d53d29a574e0ebcb1f3a36c559016
pgarbrecht <philgarbrecht@gmail.com>
2022-11-02 19:18:13 -0500

    mongodb atlas connected

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-//use express (restful api framework), assign app variable for it
+// use express (restful api framework), assign app variable for it
 const express = require('express');
 const app = express();
 // import our model
 const Products = require('./models/products.js')
 
-//SERVER MIDDLEWARE
-//use dotenv to store secret info 
+
+
+// SERVER MIDDLEWARE
+// use dotenv to store secret info 
 require('dotenv').config()
 const PORT = process.env.PORT
+// use mongDB for database, mongoose for schema
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI
+mongoose.connect(mongoURI);
+mongoose.connection.once('open', () => {
+   console.log('connected to mongo');
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('server working')
 });
 
-//express app listens on port and confirms server is running
+// express app listens on port and confirms server is running
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
